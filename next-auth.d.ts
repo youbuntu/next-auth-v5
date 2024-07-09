3dbab8cc2d810a07e58698c2fbc61a577a285d23
youbuntu <mkipeo@gmail.com>
2024-07-09 22:36:53 +0900

    Callbacks

---
+++ next-auth.d.ts
@@ -0,0 +1,19 @@
+import { UserRole } from "@prisma/client";
+import NextAuth, { type DefaultSession } from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole;
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: UserRole;
+  }
+}
